ab1738ade44249ae2873c7f5df46195b4308b536
Michelle Sauder <mgsauder@hotmail.com>
2022-02-08 23:31:09 -0800

    some css

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,41 +1,70 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import Details from '../components/Details';
+import { data } from '../data';
+import '../App.scss';
 
-const Table = ({ features }) => {
+const Table = () => {
+    const [sortedField, setSortedField] = useState()
+    const [sortAscending, setSortAscending] = useState()
+    const [sortedData, setSortedData] = useState(data.data.features)
 
-    const [sortedField, setSortedField] = useState(null);
-
-    let sortedFeatures = [...features];
-    if (sortedField !== null) {
-        sortedFeatures.sort((a, b) => {
-        if (a[sortedField] < b[sortedField]) {
-          return -1;
-        }
-        if (a[sortedField] > b[sortedField]) {
-          return 1;
-        }
-        return 0;
-      });
+    if (sortedField === undefined) {
+      sortField("place")
     }
+
+    const tableData = sortedData.map((feature) => {
+      return (
+        <tr key={feature.id}>
+          <td data-label="Title" className="primary">
+            <Link to={`/details/${feature.id}`}>
+              {feature.properties.place}</Link>
+          </td>
+          <td data-label="Magnitude">{feature.properties.mag}</td>
+          <td data-label="Time">{new Date(feature.properties.time).toUTCString()}</td>
+        </tr>
+      )
+    });
+
+    function sortField(name) {
+        setSortAscending(!sortAscending)
+        sortedData.sort((a, b) => {
+          if (a.properties[name] < b.properties[name]) {
+            return sortAscending ? 1 : -1
+          }
+          if (a.properties[name] > b.properties[name]) {
+            return sortAscending ? -1 : 1
+          }
+          return 0
+        })
     
+        setSortedField(name)
+        setSortedData(sortedData)
+      }
+    
+      function sortIndicator(fieldName) {
+        if (sortedField === fieldName) {
+          if (sortAscending) {
+            return "asc"
+          }
+          return "desc"
+        }
+      }
     return (
         <>
-        <table>
-            <tr>
-                <th onClick={() => setSortedField('title')}>Title</th>
-                <th onClick={() => setSortedField('mag')}>Magnitude</th>
-                <th onClick={() => setSortedField('time')}>Time</th>
-            </tr>
-            {sortedFeatures.map((feature)=>{
-                return (
-                        <tr>
-                            <td><Link to={`/details/${feature.id}`} >{feature.properties.place}</Link></td>
-                            <td>{feature.properties.mag}</td>
-                            <td>{feature.properties.time}</td>
-                        </tr>)
-                })}
+        <div className="App">
+            <table className="container" >
+                <th>
+                    <tr>
+                      <th onClick={() => sortField('place')} className={sortIndicator('place')}>Title</th>
+                      <th onClick={() => sortField('mag')} className={sortIndicator('mag')}>Magnitude</th>
+                      <th onClick={() => sortField('time')} className={sortIndicator('time')}>Time</th>
+                    </tr>
+                </th>
+            <tbody>
+                {tableData}
+            </tbody>
             </table>
+        </div>
         </>
     )
 }
