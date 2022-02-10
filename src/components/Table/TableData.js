34b9d2000509abdac7964022ecd347048d70e5ab
Michelle Sauder <mgsauder@hotmail.com>
2022-02-09 21:40:48 -0800

    install json loader

--- src/components/Table/TableData.js
+++ src/components/Table/TableData.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const TableData = ({ tableData }) => {
+
     var dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
     var timeOptions = { hour: '2-digit', minute: '2-digit' };
 
